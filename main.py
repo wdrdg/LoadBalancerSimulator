ef7f2d4327c4337436bea1d2a31d41aff705dccb
wdrdg <59623593+wdrdg@users.noreply.github.com>
2022-02-11 23:10:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -43,79 +43,7 @@ class LoadBalancer:
 	def __init__(self, address):
 		self.id = address
 		self.connection_table = {}
-
-	def assign_server_random(self, packet, _1, _2):
-		return random.randint(0, num_servers - 1)
-
-	def assign_server_hashing(self, packet, _1, _2):
-		return hash((packet.clientid, packet.port_num)) % num_servers
-
-	def assign_server_power_of_2_choices_no_memory(self, packet, servers, _):
-
-		#pick 2 servers randomly
-		first_query_server = random.randint(0, num_servers - 1)
-		second_query_server = random.randint(0, num_servers - 1)
-
-		#ensure 2nd server is different from 1st
-		while (first_query_server == second_query_server):
-			second_query_server = random.randint(0, num_servers - 1)
-
-		#get loads of both servers
-		first_query_load = servers[first_query_server].get_load(packet.time_sent)
-		second_query_load = servers[second_query_server].get_load(packet.time_sent)
-
-		#pick server with least load 
-		if first_query_load < second_query_load:
-			return first_query_server
-		return second_query_server
-
-	def assign_server_power_of_2_choices_with_memory(self, packet, servers, _):
-		#check this is a new flow by checking header tuple
-		header_tuple = (packet.clientid, packet.port_num)
-		if header_tuple in self.connection_table:
-			return self.connection_table[header_tuple]
-
-		#pick 2 servers randomly
-		first_query_server = random.randint(0, num_servers - 1)
-		second_query_server = random.randint(0, num_servers - 1)
-
-		#ensure 2nd server is different from 1st
-		while (first_query_server == second_query_server):
-			second_query_server = random.randint(0, len(servers) - 1)
-
-		#get loads of both servers
-		first_query_load = servers[first_query_server].get_load(packet.time_sent)
-		second_query_load = servers[second_query_server].get_load(packet.time_sent)
-
-		#pick server with least load, and store it in the connection table to ensure future consistency
-		if first_query_load < second_query_load:
-			self.connection_table[header_tuple] = first_query_server
-			return first_query_server
-		self.connection_table[header_tuple] = second_query_server
-		return second_query_server
-
-	def assign_server_power_of_x_choices_with_memory(self, packet, servers, x):
-		#check this is a new flow by checking header tuple
-		header_tuple = (packet.clientid, packet.port_num)
-		if header_tuple in self.connection_table:
-			return self.connection_table[header_tuple]
-
-		if x > num_servers:
-			print("Number of servers too low for power of " + str(x) + " choices.")
-			return
-
-		#pick x servers randomly
-		server_nums = random.sample(range(0, num_servers), x)
-		loads = []
-		for num in server_nums:
-			loads.append(servers[num].get_load(packet.time_sent))
-
-		min_load_server_id = loads.index(min(loads))
-		min_server_num = server_nums[min_load_server_id]
-
-		self.connection_table[header_tuple] = min_server_num
-		return min_server_num
-
+		
 	def __repr__(self):
 		return "Load Balancer id: " + str(self.id)	
 
@@ -172,58 +100,8 @@ class Packet:
 	def __repr__(self):
 		return "Packet from client: " + str(self.clientid) + "at port: " + str(self.port_num) +  " @time: " + str(round(self.time_sent,3))
 
-def run_load_plotter(servers, assignment_method):
-	# Post-Simulation Analysis
-	for server in servers:
-		#print(server)
-		times = []
-		loads = []
-		for t in [x/250 for x in range(1, 250)]:
-			load = server.get_load(t)
-			# print("Load at time", t, "is", round(load, 3))
-			times.append(t)
-			loads.append(load)
 
-		plt.plot(np.array([x for x in times]), np.array([y for y in loads]), label=str("Server" + str(server.id))) # , s=5
-		plt.legend(loc="best")
-		plt.xlabel('Time')
-		plt.ylabel('Load (Time til finish)')
-		plt.title('Load vs. Time for Servers')
-		plt.axis([0, 1, 0, 0.6])
-
-	plt.savefig('/content/LoadBalancerSimulator/plots/SmallSystemWithFlows/' + assignment_method + '/LoadVsTimeForServers.png')
-	plt.clf()
-
-def run_mean_and_stdev_plotter(servers, assignment_method):
-	times = []
-	means = []
-	stdevs = []
-	for t in [x/250 for x in range(1, 250)]:
-		times.append(t)
-		mean = sum([x.get_load(t) for x in servers]) / len(servers)
-		stdev = pow( sum([((x.get_load(t) - mean) ** 2) for x in servers]) / (len(servers) - 1), 1/2)
-		means.append(mean)
-		stdevs.append(stdev)
-		
-	plt.plot(np.array([x for x in times]), np.array([y for y in means]), label="mean")
-	plt.plot(np.array([x for x in times]), np.array([y for y in stdevs]), label="stdev")
-
-	avg_mean = sum(means)/len(means)
-	avg_stdev = sum(stdevs)/len(stdevs)
-
-	print("Mean load: %5.3f" % avg_mean)
-	print("Mean standard deviation: %5.3f" % avg_stdev)
-
-	plt.legend(loc="best")
-	plt.xlabel('Time')
-	plt.ylabel('Mean / stdev server load')
-	plt.title('Mean and Stdev of Load vs. Time for Servers')
-	plt.axis([0, 1, 0, 0.6])
-
-	plt.savefig('/content/LoadBalancerSimulator/plots/SmallSystemWithFlows/' + assignment_method + '/MeanAndStdevLoadVsTimeForServers.png')
-	plt.clf()
-
-def run_consistency_check(servers):
+	def run_consistency_check(servers):
 	perFlowConsistent = True
 	for server in servers:
 		for otherServer in servers:
@@ -247,11 +125,11 @@ def run_simulation(assignment_method):
 	packets = []
 	for i in range(num_flows):
 		num_packets_in_flow = int(np.random.normal(mean_flow_size, flow_size_stdev))
-		client_of_flow = random.randint(0, num_clients-1)
+		client_of_flow = random.randint(0, num_clients-1) # client id -- random????
 		time_of_flow = random.random()
 		port_number = random.randint(1024, 65536)
 		for j in range(num_packets_in_flow):
-			time_of_packet = time_of_flow + (random.random() * (max_flow_duration) - max_flow_duration / 2)
+			time_of_packet = time_of_flow + (random.random() * (max_flow_duration) - max_flow_duration / 2) # why ????
 			packet = Packet(client_of_flow, port_number, time_of_packet)
 			packets.append(packet)
 	packets.sort(key=lambda x: x.time_sent, reverse=False)
@@ -270,15 +148,15 @@ def run_simulation(assignment_method):
 	for i in range(len(packets)):
 		packet = packets[i]
 
-		if LOAD_BALANCER_DROPS:
+		if LOAD_BALANCER_DROPS: 
 			if i < len(packets)/2:
 				lb_id = packet.clientid % num_load_balancers
 			else: #last load balancer "goes down"
 				lb_id = packet.clientid % (num_load_balancers - 1)
 		else:
-			lb_id = packet.clientid % num_load_balancers
+			lb_id = packet.clientid % num_load_balancers #???????? 
 
-		load_balancer = load_balancers[lb_id]
+		load_balancer = load_balancers[lb_id] # choose the balancer randomly????
 		switcher = {
 			"RandomAssignment": load_balancer.assign_server_random,  # No per-flow consistency :(
 			"ConsistentHashing": load_balancer.assign_server_hashing, # Per-flow consistency :)
